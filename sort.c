cdfddb97a949ba23eb9f5e5480927664152c2725
IceMan-26 <134019738+IceMan-26@users.noreply.github.com>
2023-07-14 18:57:48 -0400

    commit

--- sort.c
+++ sort.c
@@ -5,16 +5,108 @@
 int extraMemoryAllocated;
 
 // implements heap sort
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        heapify(arr, n, largest);
+    }
+}
+
+
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+	for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        heapify(arr, i, 0);
+    }
+
 }
 
 
 // implement merge sort
+void merge(int arr[], int l, int m, int r){
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	/* create temp arrays */
+	int *L = (int*) malloc(n1*sizeof(int));
+	int *R = (int*) malloc(n2*sizeof(int));
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1+ j];
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+
+	while (i < n1 && j < n2){
+		if (L[i] <= R[j]){
+			arr[k] = L[i];
+			i++;
+		}
+		else{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* Copy the remaining elements of L[], if there
+	are any */
+	while (i < n1){
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	/* Copy the remaining elements of R[], if there
+	are any */
+	while (j < n2){
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+
+	free(L);
+	free(R);
+}
+
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+	if(l < r){
+
+		int m = (l + r) / 2;
+
+		mergeSort(pData, l, m);
+		mergeSort(pData, m + 1, r);
+
+		merge(pData, l, m, r);
+	}
+
 }
 
 // parses input file to an integer array
